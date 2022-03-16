a3c68c5c4a5d9462fc152e0686210207abd9d582
caroljfvufhebvy <anacarolinemartineli02@gmail.com>
2022-03-16 08:00:36 -0300

    salvar

---
+++ exercicio1.php
@@ -0,0 +1,5 @@
+<?php
+$dia = 10;
+$quinzena = ($dia<=15)?"1º quinzena":"2º quinzena";
+echo $dia." pertence a ",$quinzena;
+?>
---
+++ exercicio2.php
@@ -0,0 +1,15 @@
+<?php
+
+$dia = 15;
+
+if ($dia>15){
+    echo "2º quinzena";
+} 
+else if ($dia==15){
+    echo "Meio do mês";
+}
+else{
+    echo "1º quinzena";
+}
+
+?>
