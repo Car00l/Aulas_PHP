ade89e5a12b5bdd723b6654bd79dcef999f45d58
Car00l <anacarolinemartineli02@gmail.com>
2022-06-22 21:01:28 -0300

    prova

---
+++ h.php
@@ -0,0 +1,20 @@
+<?php
+$contador = 0;
+
+while($sorteio != 37){
+    $sorteio = rand(0, 330);
+    echo $sorteio ." <br>";
+    $contador++;
+    }
+?>
+
+<?php
+
+$contador = 0;
+
+while(){
+if (){
+    
+}
+$contador++;
+}
